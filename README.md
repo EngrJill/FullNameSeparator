938ccefe458b9d55b28b482490de56aa71fe715d
Daniel A. Soriano II <72549944+EngrJill@users.noreply.github.com>
2021-08-12 10:28:54 +0800

    ITR1 Finished

---
+++ README.md
@@ -0,0 +1,10 @@
+#Direction for Use
+
+If you forgot to separate the forms for First Name, Middle Name and Last Name, then you can use this.
+
+##It only works with
+
+This code only works with Full Names (e.g.: Juan Santos Cruz or Juan Angelo Santos Cruz up
+to 4 First Names)
+
+It uses the spaces algorithm to determine FN, MN, LN
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Remove Commas and dots in the full names
 def remove_commas_dots(arr):
     NewArr = []
 
@@ -11,7 +12,7 @@ def remove_commas_dots(arr):
 
     return NewArr
 
-
+#Convert Word to array
 def String_to_Array(arr):
     arr2 = []
     for i in range(len(arr)):
@@ -29,32 +30,45 @@ def Sentence_Case(arr):
 def Separate(arr):
     NewArr = []
     for i in range (len(arr)):
-            if (len(arr[i]) == 3):
-                NewArr.append(["First Name: " + arr[i][0], "Middle Name: " + arr[i][1], "Last Name: " + arr[i][2]])
+            if (len(arr[i]) == 2):
+                NewArr.append([arr[i][0] +",", arr[i][1] + "\n"])
+
+            elif (len(arr[i]) == 3):
+                NewArr.append([arr[i][0] +",", arr[i][1] +",",arr[i][2] + "\n"])
+
+            elif (len(arr[i]) == 4):
+                NewArr.append([arr[i][0] + " " + arr[i][1] +",", arr[i][2] +",", arr[i][3]+ "\n"])
+
+            elif (len(arr[i]) == 5):
+                NewArr.append([arr[i][0] + " " + arr[i][1] + " " + arr[i][2] +",", arr[i][3] + ",", arr[i][4] + "\n"])
+
+            elif (len(arr[i]) == 6):
+                NewArr.append([arr[i][0]  + " " + arr[i][1]  + " " + arr[i][2] + " " + arr[i][3] +"," ,arr[i][4] +",",arr[i][5]+ "\n"])
 
     return NewArr
 
+def Arr_To_String_1(arr):
+    newArr = []
+    names = ""
+    for i in range(len(arr)):
+        newArr.append(names.join(arr[i]))
+
+    return newArr
+
+def Arr_To_String_2(arr):
+        names = ""
+        return names.join(arr)
 
 
-# print(String_to_Array(Sentence_Case(remove_commas_dots(["JOSEPH sales ORtiZ","Joanna Denise Flores Dela Cruz",
-#                                         "Shiela Mae Broñosa Reodique","Zabdiel Abarquez Agsunod",
-#                                         "Kenneth John Bejo","Francia Guzman Monforte","Carla Salanguste Epino",
-#                                         "Ranneil Ariban Rivera","Joanna Denise Flores Dela Cruz",
-#                                         "Joseph Sales Ortiz","Raymart Gullem Fulo","Leamae, De Lemios, Germo",
-#                                         "Sheana May Palma Bunao","Clayton Lawrence Buarao Beato",
-#                                         "Mark Guadalupe Advincula","Kenneth Francis, Del Rosario, Felices",
-#                                         "Ranier Abe Mirabueno. frial","Cyril Audrey Ambrocio Relato",
-#                                         "Rose Antonette Estrella Paez","Kim John Sario Atento",
-#                                         "Ma. Fe Anthonette A. Castro","Kimberly Loria Lovendino",
-#                                         "Russel, Cañete, Bitancur","Mary Grace Cornel Oprecio",
-#                                         "Ivan Cris Bonita Montealegre","John Dave R. Marbella",
-#                                         "Joshua, Nipa, Anobling","Marc Lenray, Nacion, Magdaraog",
-#                                         "Jazpher Jhay, Flores, Salazar","Jemuel Jireh Lanuzo Berunio",
-#                                         "Lorraine Velasco Abache","Danielle Marie Igay","Kim John Sario Atento",
-#                                         "Rose Antonette E. Paez","Carla Salanguste Epino",
-#                                         "Lorraine Velasco Abache","Daniel Aclan Soriano II"]))))
 
-print(Separate(String_to_Array(Sentence_Case(remove_commas_dots(["joseph ortis sdads", "joasdn asdha asdhads"])))))
+print(Arr_To_String_2(Arr_To_String_1
+                      (Separate(String_to_Array(Sentence_Case(remove_commas_dots
+                                                              ()))))))
+
+
+
+
+
 
 
 
